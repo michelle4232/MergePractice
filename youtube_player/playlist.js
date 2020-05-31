b869f8689bd1b2e82ac83c13aeb0ce055f16e624
michelle4232 <michelle4232@gmai.com>
2020-05-31 22:53:38 +0800

    What I have done at this time

--- youtube_player/playlist.js
+++ youtube_player/playlist.js
@@ -2,14 +2,14 @@
 //YouTube影片ID
 
 let playList =[
-    "TgOu00Mf3kI",//讓我留在你身邊
-    "gmgcRWxhmqY",//全放空
-    "mtAc_bMYBsM"//飄向北方
+    "pcKR0LPwoYs",//讓我留在你身邊
+    "W0cs6ciCt_k",//全放空
+    "UFFFu0UAcOQ"//飄向北方
 ];
 
 //播放起訖秒數
 let playTime = [
-    [57,60],
-    [79,83],
-    [76,80]
+    [57,63],
+    [79,85],
+    [42,52]
 ]
