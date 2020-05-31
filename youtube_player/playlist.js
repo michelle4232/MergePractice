4b0ff8d220e442b6aac32a8d2c32b0df907674c4
michelle4232 <michelle4232@gmai.com>
2020-05-31 22:59:44 +0800

    the

--- youtube_player/playlist.js
+++ youtube_player/playlist.js
@@ -2,15 +2,10 @@
 //YouTube影片ID
 
 let playList =[
-<<<<<<< HEAD
     "pcKR0LPwoYs",//讓我留在你身邊
     "W0cs6ciCt_k",//全放空
     "UFFFu0UAcOQ"//飄向北方
-=======
-    "TgOu00Mf3kI",//讓我留在你身邊
-    "gmgcRWxhmqY",//全放空
-    "mtAc_bMYBsM"//飄向北方
->>>>>>> aec4efc5b936cc98e5f4abc170aeaaa497f289b5
+
 ];
 
 //播放起訖秒數
